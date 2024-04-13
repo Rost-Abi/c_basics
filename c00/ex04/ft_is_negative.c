cad8170d12fb4978568383da3d598df0ceac293d
Abi <rostomyanabel7@gmail.com>
2024-04-13 22:08:08 +0400

    ft_is_negative done

--- c00/ex04/ft_is_negative.c
+++ c00/ex04/ft_is_negative.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abi <abi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/13 21:36:00 by abi               #+#    #+#             */
-/*   Updated: 2024/04/13 21:36:04 by abi              ###   ########.fr       */
+/*   Created: 2024/04/13 22:02:02 by abi               #+#    #+#             */
+/*   Updated: 2024/04/13 22:06:38 by abi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <unistd.h>
 
-void ft_putchar(char c)
+void ft_is_negative(int n)
 {
+    char c;
+    if (n < 0)
+        c = 'N';
+    else
+        c = 'P';
     write(1, &c, 1);
 }
 
-// int main(){
-//     ft_putchar('h');
-//     ft_putchar('\n');
-// }
+int main()
+{
+    ft_is_negative(1);
+    ft_is_negative(-23);
+    ft_is_negative(0);
+}
